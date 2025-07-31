840cd1841f017ae5436a342f3855a07ad91ab47d
DESKTOP-20UV94O\PC <naspodkop@gmail.com>
2025-07-31 14:50:51 +0300

    Доработка проекта

--- cities.py
+++ cities.py
@@ -8,7 +8,7 @@ def get_coordinates(city, key):
         if results:
             lat = round(results[0]['geometry']['lat'], 2)
             lon = round(results[0]['geometry']['lng'], 2)
-            return lat, lon
+            return f'Широта: {lat}, Долгота: {lon}'
         else:
             return 'Город не найден'
     except Exception as e:
